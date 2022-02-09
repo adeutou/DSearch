a89a0628f12bd5ec285bbb46f3877f07eccf8fde
adeutou <a.deutou@gmail.com>
2022-02-09 22:29:43 +0100

    responsive design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,15 +6,15 @@ import { Search } from './Search';
 export const Navbar = ({ darkTheme, setDarkTheme}) => {
   return (
     <div className='p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200'>
-        <div className='flex justify-between items-center space-x-5 w-screen'>
+        <div className='flex justify-between items-center space-x-5 w-screen' id="navBar">
             <Link to='/'>
                 <p className='text-4xl font-bold text-blue-500 py-1 px-2 dark:text-white'>
                     Dsearch
                 </p>
             </Link>
             <Search />
-            <button type='button' onClick={() => setDarkTheme(!darkTheme)} className="text-xl text-white dark:bg-white dark:text-gray-900 bg-gray-800 border rounded-full px-2 py-1 hover:shadow-lg" >
-                {darkTheme ? 'Light 💡' : 'Dark 🌙'}
+            <button type='button' id="selectMode" onClick={() => setDarkTheme(!darkTheme)} className="text-xl text-white dark:bg-white dark:text-gray-900 bg-gray-800 border rounded-full px-2 py-1 hover:shadow-lg" >
+                {darkTheme ? 'Light💡' : 'Dark🌙'}
             </button>
         </div>
         
